aee5cd826e969b358e327e5622247c7b6658c26e
Mickeydev <michaeil.zare1382@gmail.com>
2024-08-16 12:08:11 +0330

    Create Module and Service User Then User RequestBody

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, RawBodyRequest, Req } from '@nestjs/common';
+import { Request } from 'express';
+import { UsersService } from "./users.service";
 
 @Controller('users')
 export class UsersController {
+    constructor(private readonly _userService:UsersService){}
     @Get("detail")
     getUser ():string {
-        return "User Detail..."
+        return this._userService.getDetail()
+    }
+
+    @Post("/:name")
+    create(@Req() req:RawBodyRequest<Request>):string{
+        return req.params.name
     }
 }
